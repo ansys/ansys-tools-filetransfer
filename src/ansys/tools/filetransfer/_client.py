319d42bc5fdfc70535cdd32a749a0d59a1fbfb73
Dominik Gresch <dominik.gresch@ansys.com>
2026-01-12 14:44:05 +0100

    Update license header year

--- src/ansys/tools/filetransfer/_client.py
+++ src/ansys/tools/filetransfer/_client.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2022 - 2025 ANSYS, Inc. and/or its affiliates.
+# Copyright (C) 2022 - 2026 ANSYS, Inc. and/or its affiliates.
 # Copyright 2022 ANSYS, Inc. Unauthorized use, distribution, or duplication is prohibited.
 # SPDX-License-Identifier: MIT
 #
